13effdcca6880ce5eea350efc3626b2d03f6948c
Raghul Nanth A <nanth.raghul@gmail.com>
2022-02-18 23:15:43 +0530

    feat: Better bottom nav

--- cms/templates/common/bottomnav/assets/js/bottom-side-nav.js
+++ cms/templates/common/bottomnav/assets/js/bottom-side-nav.js
@@ -1,43 +1,61 @@
-const events = ["EWITTS", "PHRONESIS", "PANDORA'S BOX", "ROBOREX", "CONCEPTION", "BYTEHOC", "CONCREATE", "MANIGMA"]
+const events = [
+  "EWITTS",
+  "PHRONESIS",
+  "PANDORA'S BOX",
+  "ROBOREX",
+  "CONCEPTION",
+  "BYTEHOC",
+  "CONCREATE",
+  "MANIGMA",
+];
 
-const current = "MANIGMA"
+const current = "MANIGMA";
 const sidenav = document.querySelector(".sidenav");
+const content = document.querySelector("#content");
+
+content.innerHTML += events
+  .map((x) => {
+    if (x !== current) return `<a href='#' class='options'>${x}</a>`;
+    else return "";
+  })
+  .join("\n");
+
+sidenav.innerHTML = `<div id="hello" onclick="change()"><span id="cur">${current}</span> <img id="dropup-button" style="margin-left: 10px" src="./assets/images/right.png"></div>`;
+
+
 let open = false;
 
 const change = () => {
-  const blur = document.querySelector("#blur");
   const content = document.querySelector("#content");
-  if(open) {
-    document.querySelector("#dropup-button").classList.remove("checked");
-    document.querySelectorAll(".sidenav a").forEach(x => {
-      x.style.display = "none";
-      sidenav.style.position = "";
-    })
-    sidenav.classList.remove("sidenav-open")
-    sidenav.style["z-index"] = 0;
-    blur.classList.remove("overlay");
-    content.style.display = "none";
-  } else {
+  const total = document.querySelector(".total");
+  const drop_btn = document.querySelector("#dropup-button");
+  const menu = document.querySelector(".menu-navigation-icons");
+  const content_a = document.querySelectorAll("#content a");
+  if (!open) {
     content.style.display = "flex";
-    sidenav.classList.add("sidenav-open")
-    sidenav.style["z-index"] = 1;
-    sidenav.style.position = "fixed";
-    sidenav.style.bottom = 0;
-    sidenav.style.left = 0;
-    document.querySelectorAll(".sidenav a").forEach(x => {
-      x.style.display = "block";
+    drop_btn.classList.add("checked");
+    total.classList.add("sidenav-open");
+
+    setTimeout(() => {
+      content_a.forEach((x) => {
+        x.style.opacity = "1";
+      });
+    }, 10);
+    menu.classList.add("sidenav-open");
+  } else {
+    content_a.forEach((x) => {
+      x.style.opacity = "0";
+    });
+    content_a[0].addEventListener("transitionend", () => {
+      content.style.display = "none";
+      total.classList.remove("sidenav-open");
+      drop_btn.classList.remove("checked");
+    }, {
+      capture: false,
+      once: true,
+      passive: false,
     })
-    document.querySelector("#dropup-button").classList.add("checked");
-    blur.classList.add("overlay");
+    // menu.classList.remove("sidenav-open");
   }
   open = !open;
-}
-
-sidenav.innerHTML = `<div id="content">${(events.map(x => {
-  if(x !== current)
-    return `<a href='#' class='options'>${x}</a>`;
-  else
-    return "";
-})).join("\n")}</div>`;
-
-sidenav.innerHTML += `<div id="hello" onclick="change()"><span id="cur">${current}</span> <img id="dropup-button" style="margin-left: 10px" src="../images/right.png"></div>`
+};
